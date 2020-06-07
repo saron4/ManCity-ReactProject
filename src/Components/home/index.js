665b3e7b0ad867a22c80a57b465fe387e470bcba
saron4 <saronfantahun9@gmail.com>
2020-06-07 13:33:06 -0500

    done footer and animation

---
+++ src/Components/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Featured from './featured';
+import Maches from './maches';
+
+const Home = () => {
+    return (
+        <div className="bck_blue">
+            <Featured/>
+            <Maches/>
+        </div>
+    );
+};
+
+export default Home;
--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import Header from '../Components/Header_footer/Header';
-//import Footer from '../Components/Header_footer/Footer.js';
+import Footer from '../Components/Header_footer/Footer';
 
 const Layout = (props) => {
     return (
         <div>
             <Header/>
             {props.children}
-           
+            <Footer/>
         </div>
     );
 };
 
 export default Layout;
-
-
-
