d24dad1cb6052a0e5a57da81dcf6f5892c62693a
saron4 <saronfantahun9@gmail.com>
2020-06-21 15:00:04 -0500

    full file

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app'
 import 'firebase/app'
 import 'firebase/database'
+import 'firebase/auth'
+import 'firebase/storage'
 
 var firebaseConfig = {
   apiKey: "AIzaSyBBGJ6cfwMuLo538ZlS6qrjfQW1RxtUui0",
@@ -12,15 +14,23 @@ var firebaseConfig = {
   appId: "1:64298506235:web:3d2f603e85da5dad745d5e",
   measurementId: "G-2RVQ1P9L8L"
 };
-
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref('matches');
 const firebasePromotions = firebaseDB.ref('promotions');
+const firebaseTeams = firebaseDB.ref('teams');
+const firebasePlayers = firebaseDB.ref('players');
 
 export {
     firebase,
     firebaseMatches,
-    firebasePromotions
+    firebasePromotions,
+    firebaseTeams,
+    firebasePlayers,
+    firebaseDB
+
+
 }
